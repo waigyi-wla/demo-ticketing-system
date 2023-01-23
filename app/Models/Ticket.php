486dab95a5a381b3333cecaa2d39092451626879
Wai Lwin Aung <67897857+waigyi-wla@users.noreply.github.com>
2023-01-23 16:52:48 +0630

    first commit

---
+++ app/Models/Ticket.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'category_id', 'title', 'priority', 'description', 'status'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
